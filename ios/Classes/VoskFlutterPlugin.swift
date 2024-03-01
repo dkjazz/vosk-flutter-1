edbb9390ab9d3d4738d545b642ac1ba07031417f
Dmitry Kravtsov <39170109+dkjazz@users.noreply.github.com>
2024-02-29 19:46:14 -0800

    Got libvosk.a copied

--- ios/Classes/VoskFlutterPlugin.swift
+++ ios/Classes/VoskFlutterPlugin.swift
@@ -3,7 +3,7 @@ import UIKit
 
 public class VoskFlutterPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    setenv("LIBVOSK_PATH", Bundle.main.privateFrameworksPath! + "/libvosk.framework/libvosk", 0);
+    setenv("LIBVOSK_PATH", Bundle.main.privateFrameworksPath! + "/libvosk.a", 0);
 
     let channel = FlutterMethodChannel(name: "vosk_flutter", binaryMessenger: registrar.messenger())
     let instance = VoskFlutterPlugin()
