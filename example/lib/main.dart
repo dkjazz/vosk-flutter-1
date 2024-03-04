781377a858af83274461f8fe9fac2e85f1224c0d
Dmitry Kravtsov <39170109+dkjazz@users.noreply.github.com>
2024-03-03 20:06:17 -0800

    Update packages

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:mic_stream/mic_stream.dart';
 import 'package:record/record.dart';
 import 'package:vosk_flutter/vosk_flutter.dart';
 import 'package:path_provider/path_provider.dart';
